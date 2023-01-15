6e639642f35efa87feff5950fff7c36c67b6a102
Fabiany Lopes <fabiany.lopes.060@ufrn.edu.br>
2023-01-15 12:13:19 -0300

    done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        List<String> fruits = new ArrayList<>();
+        fruits.add("pera");
+        fruits.add("uva");
+        fruits.add("maçã");
+        fruits.add("melancia");
+
+        List<String> mFruits = new ArrayList<>();
+        mFruits.add("melancia");
+        mFruits.add("manga");
+        mFruits.add("morango");
+        mFruits.add("mamão");
+
+        for (int i = 0; i < mFruits.size(); i++) {
+            if (fruits.contains(mFruits.get(i))) {
+                System.out.println(mFruits.get(i));
+            }
+        }
     }
 }
